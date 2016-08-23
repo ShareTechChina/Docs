024daa90409f11559aadf58c13f54b28000e91d4
znzhang1985 <znzhang1985@163.com>
2016-08-23 10:02:25 +0800

    Update duoshouji_view.sql

--- sql-script/duoshouji_view.sql
+++ sql-script/duoshouji_view.sql
@@ -1,15 +1,18 @@
-create view squer_notes AS
-select
-  c.id note_id
-	, c.title
-	, u.id owner_id
-	, u.avatar_url portrait
-	, c.image1 image
-	, c.image1_width image_width
-	, c.image1_height image_height
-	, a1.like_number like_count
-	, a1.comment_number comment_count
-	, a2.avg_rank rank
-  , c.create_time created_time
-from note c, note_extend a1, (select note_id, avg(value) avg_rank from rating group by note_id) a2, user u
+
+drop view if exists duoshouji.v_user;
+
+create view duoshouji.v_user as
+select c.mobile, c.user_name, c.password, c.avatar_url, c.avatar_width, c.avatar_height, a.token
+from duoshouji.user c, duoshouji.user_wechat_login a
+where c.id = a.user_id
+;
+
+
+drop view if exists duoshouji.v_square_notes;
+
+create view duoshouji.v_square_notes as
+select c.id, c.title, a1.rank, c.image1, c.image1_width, c.image1_height, c.create_time, a2.comment_number, a2.like_number, a2.order_number,
+  u.mobile, u.user_name, u.avatar_url, u.avatar_width, u.avatar_height
+from duoshouji.note c, (select note_id, avg(value) rank from duoshouji.rating group by note_id) a1, duoshouji.note_extend a2, duoshouji.user u
 where c.id = a1.note_id and c.id = a2.note_id and c.user_id = u.id
+;
