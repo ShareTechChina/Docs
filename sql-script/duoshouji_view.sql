5c6973f65bf8649645ea991f5d7e9588b4569588
znzhang1985 <znzhang1985@163.com>
2016-08-25 17:32:40 +0800

    Update duoshouji_view.sql

--- sql-script/duoshouji_view.sql
+++ sql-script/duoshouji_view.sql
@@ -7,11 +7,13 @@ from duoshouji.user
 ;
 
 
+
 drop view if exists duoshouji.v_square_notes;
 
 create view duoshouji.v_square_notes as
 select c.id, c.title, a1.rank, c.image1, c.image1_width, c.image1_height, c.create_time, a2.comment_number, a2.like_number, a2.order_number,
   u.mobile, u.user_name, u.avatar_url, u.avatar_width, u.avatar_height
-from duoshouji.note c, (select note_id, avg(value) rank from duoshouji.rating group by note_id) a1, duoshouji.note_extend a2, duoshouji.user u
-where c.id = a1.note_id and c.id = a2.note_id and c.user_id = u.id
-;
+from duoshouji.note c left join
+  (select note_id, avg(value) rank from duoshouji.rating group by note_id) a1 on c.id = a1.note_id left join
+  duoshouji.note_extend a2 on c.id = a2.note_id left join
+  duoshouji.user u on c.user_id = u.id
