7e795235ad13e553aa7ddc1dad128f202940d67f
znzhang1985 <znzhang1985@163.com>
2016-09-11 15:46:55 +0800

    Update insert-data.sql

--- sql-script/insert-data.sql
+++ sql-script/insert-data.sql
@@ -3,20 +3,21 @@ insert into duoshouji.tag (name, is_chanel, sort_order) values ('美人志', 1, 
 insert into duoshouji.tag (name, is_chanel, sort_order) values ('宝贝书', 1, 3);
 insert into duoshouji.tag (name, is_chanel, sort_order) values ('吃货说', 1, 4);
 
-insert into category (name) values ('女装男装');
-insert into category (name) values ('鞋类箱包');
-insert into category (name) values ('母婴用品');
-insert into category (name) values ('护肤彩妆');
-insert into category (name) values ('汇吃美食');
-insert into category (name) values ('珠宝配饰');
-insert into category (name) values ('家装建材');
-insert into category (name) values ('家居家纺');
-insert into category (name) values ('百货市场');
-insert into category (name) values ('汽车用品');
-insert into category (name) values ('手机数码');
-insert into category (name) values ('家电办公');
-insert into category (name) values ('更多服务');
-insert into category (name) values ('生活服务');
-insert into category (name) values ('运动户外');
-insert into category (name) values ('花鸟文娱');
-insert into category (name) values ('农资采购');
+insert into duoshouji.category (category_id, category_name) values (10000000, '女装男装');
+insert into duoshouji.category (category_id, category_name) values (20000000, '鞋类箱包');
+insert into duoshouji.category (category_id, category_name) values (30000000, '母婴用品');
+insert into duoshouji.category (category_id, category_name) values (40000000, '护肤彩妆');
+insert into duoshouji.category (category_id, category_name) values (50000000, '汇吃美食');
+insert into duoshouji.category (category_id, category_name) values (60000000, '珠宝配饰');
+insert into duoshouji.category (category_id, category_name) values (70000000, '家装建材');
+insert into duoshouji.category (category_id, category_name) values (80000000, '家居家纺');
+insert into duoshouji.category (category_id, category_name) values (90000000, '百货市场');
+insert into duoshouji.category (category_id, category_name) values (10000000, '汽车用品');
+insert into duoshouji.category (category_id, category_name) values (11000000, '手机数码');
+insert into duoshouji.category (category_id, category_name) values (12000000, '家电办公');
+insert into duoshouji.category (category_id, category_name) values (13000000, '更多服务');
+insert into duoshouji.category (category_id, category_name) values (14000000, '生活服务');
+insert into duoshouji.category (category_id, category_name) values (15000000, '运动户外');
+insert into duoshouji.category (category_id, category_name) values (16000000, '花鸟文娱');
+insert into duoshouji.category (category_id, category_name) values (17000000, '农资采购');
+
